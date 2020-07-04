886bf67a75fb2394ebc42fb00c9d54375f23083b
Lola <lola@egherman.com>
2020-07-04 12:15:32 -0500

    add ingredients and contains

--- ranch/brands.py
+++ ranch/brands.py
@@ -18,6 +18,14 @@ brands = {
             'calcium': '8mg',
             'iron': '0mg',
             'potassium': '8mg'
-        }
+        },
+        'ingredients': ['Vegetable Oil (Soybean and/or Canola)', 'Water', 'Sugar',
+                        'Salt', 'Nonfat Buttermilk', 'Egg Yolk', 'Natural Flavors',
+                        'Spices', 'Dried Garlic', 'Dried Onion', 'Vinegar', 'Phosphoric Acid',
+                        'Xanthan Gum', 'Modified Food Starch', 'Monosodium Glutamate',
+                        'Artificial Flavors', 'Disodium Phosphate',
+                        'Sorbic Acid and Calcium Disodium EDTA Added to Preserve Freshness',
+                        'Disodium Inosinate & Guanylate'],
+        'contains': ['Milk', 'Soy', 'Egg']
     }
 }
