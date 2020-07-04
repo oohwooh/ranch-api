a0c454e8d84b7e98a3a9b87fa877fe9cc6967d96
Lola <lola@egherman.com>
2020-07-04 12:06:57 -0500

    create ranch module

---
+++ ranch/brands.py
@@ -0,0 +1,23 @@
+brands = {
+    'Hidden Valley Ranch': {
+        'description': 'Hidden Valley Original Ranch is well balanced,\
+         with subtle yet present flavors of garlic, onion, and black pepper.',
+        'nutrition': {
+            'serving_size': '2 tbsp (30mL)',
+            'calories_per_serving': 130,
+            'total_fat': '14g',
+            'saturated_fat': '2g',
+            'trans_fat': '0g',
+            'cholesterol': '5mg',
+            'sodium': '260mg',
+            'total_carbohydrate': '2g',
+            'fiber': '0g',
+            'total_sugars': '1g',
+            'protein': '0g',
+            'vitamin_d': '0mcg',
+            'calcium': '8mg',
+            'iron': '0mg',
+            'potassium': '8mg'
+        }
+    }
+}
