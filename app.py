6459113fb0f18b2f965ddbf3f1ce901992f4b388
Lola <lola@egherman.com>
2020-07-04 12:10:36 -0500

    add brand functionality

---
+++ app.py
@@ -0,0 +1,19 @@
+import random
+
+from flask import Flask
+
+import ranch
+
+app = Flask(__name__)
+
+
+@app.route('/brands')
+def list_brands():
+    """Returns a dict of ranch brands"""
+    return ranch.brands
+
+
+@app.route('/brands/random')
+def random_brand():
+    """Returns a random ranch brand"""
+    return random.choice(ranch.brands)
