4ad0d7230164846635857d717d2d18e1c44a4b56
Lola <lola@egherman.com>
2020-07-04 12:37:13 -0500

    add newman's own

--- ranch/brands.py
+++ ranch/brands.py
@@ -4,7 +4,7 @@ brands = {
         'description': 'Hidden Valley Original Ranch is well balanced, '\
         'with subtle yet present flavors of garlic, onion, and black pepper.',
         'nutrition': {
-            'serving_size': '2 tbsp (30mL)',
+            'serving_size': '2 Tbsp (30mL)',
             'calories_per_serving': 130,
             'total_fat': '14g',
             'saturated_fat': '2g',
@@ -28,5 +28,58 @@ brands = {
                         'Sorbic Acid and Calcium Disodium EDTA Added to Preserve Freshness',
                         'Disodium Inosinate & Guanylate'],
         'contains': ['Milk', 'Soy', 'Egg']
+    },
+    'Newman\'s Own Ranch': {
+        'name': 'Newman\'s Own Ranch',
+        'description': 'Newman\'s Own Ranch is a very flarvorful ranch with a prominent garlic flavor in '\
+        'conjunction with hints of onion and lemon.',
+        'nutrition': {
+            'serving_size': '2 Tbsp (30g)',
+            'calories_per_serving': 130,
+            'total_fat': '14g',
+            'saturated_fat': '1g',
+            'trans_fat': '0g',
+            'cholesterol': '5mg',
+            'sodium': '240mg',
+            'total_carbohydrate': '2g',
+            'fiber': '0g',
+            'total_sugars': '2g',
+            'protein': '0g',
+            'vitamin_d': '0mcg',
+            'calcium': '4mg',
+            'iron': '0mg',
+            'potassium': '0mg'
+        },
+        'ingredients': ['Canola Oil', 'Water', 'Buttermilk', 'Sugar', 'Distilled Vinegar',
+                        'Whole Egg (Eggs, Salt)', 'Sea Salt', 'Dried Garlic', 'Dried Onion',
+                        'Lactic Acid', 'Natural Flavor', 'Xanthan Gum (As A Thickener)',
+                        'Garlic', 'Onion Puree (Onion, Vinegar, Salt)', 'Black Pepper',
+                        'Mustard Flour', 'Dried Parsley', 'Dried Oregano', 'Lemon Juice Concentrate'],
+        'contains': ['Egg', 'Milk']
     }
 }
+
+# Template
+# 'Ranch': {
+#         'name': 'Ranch',
+#         'description': '',
+#         'nutrition': {
+#             'serving_size': '',
+#             'calories_per_serving': int,
+#             'total_fat': '',
+#             'saturated_fat': '',
+#             'trans_fat': '',
+#             'cholesterol': '',
+#             'sodium': '',
+#             'total_carbohydrate': '',
+#             'fiber': '',
+#             'total_sugars': '',
+#             'protein': '',
+#             'vitamin_d': '',
+#             'calcium': '',
+#             'iron': '',
+#             'potassium': ''
+#         },
+#         'ingredients': [],
+#         'contains': []
+#     }
